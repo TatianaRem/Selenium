e87ff3f039e160f53b5f85c5d04e37eb07e3a722
Tatiana Rem <tatiana13d@yandex.ru>
2023-04-06 19:06:17 +0600

    CSS Selectors were modified

--- src/test/java/ApplicationForDebitCardTest.java
+++ src/test/java/ApplicationForDebitCardTest.java
@@ -83,7 +83,7 @@ public class ApplicationForDebitCardTest {
         driver.findElement(By.cssSelector(".button")).click();
 
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.cssSelector(".input_type_text .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
         assertEquals(expected, actual);
 
     }
@@ -95,7 +95,7 @@ public class ApplicationForDebitCardTest {
         driver.findElement(By.cssSelector(".button")).click();
 
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
         assertEquals(expected, actual);
 
     }
@@ -107,7 +107,7 @@ public class ApplicationForDebitCardTest {
         driver.findElement(By.cssSelector(".button")).click();
 
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
         assertEquals(expected, actual);
 
     }
@@ -119,7 +119,7 @@ public class ApplicationForDebitCardTest {
         driver.findElement(By.cssSelector(".button")).click();
 
         String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
-        String actual = driver.findElement(By.cssSelector(".checkbox__text")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid .checkbox__text")).getText().trim();
         assertEquals(expected, actual);
 
     }
@@ -132,10 +132,8 @@ public class ApplicationForDebitCardTest {
         driver.findElement(By.cssSelector(".button")).click();
 
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-        String actual = driver.findElement(By.cssSelector(".input_invalid .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
         assertEquals(expected, actual);
 
     }
-
-
 }
